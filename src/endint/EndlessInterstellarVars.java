1f084da7e99f4c8ac322c7f1c0be215827ac2a6a
Zelaux <ura20060605@gmail.com>
2022-08-03 04:11:26 +0300

    Updated TemperatureController.updateBuilding

--- src/endint/EndlessInterstellarVars.java
+++ src/endint/EndlessInterstellarVars.java
@@ -1,5 +1,4 @@
 package endint;
 
 public class EndlessInterstellarVars {
-    public static float temperatureRegressionPerSecond = 0.5f;
 }
--- src/endint/tools/TemperatureController.java
+++ src/endint/tools/TemperatureController.java
@@ -1,32 +1,35 @@
 package endint.tools;
 
-import endint.*;
+import arc.util.*;
 import endint.type.*;
 import mindustry.*;
 import mindustry.gen.*;
 
 public class TemperatureController{
+    public static float temperatureRegressionPerTic = 0.5f / Time.toSeconds;
+    private static Building owner;
+
     public static <T extends Building&Temperaturec> void updateBuilding(T building){
         int range = building.temperatureRange();
-        Building owner;
         float delta;
         for(int x = -range + 1; x < range; x++){
             for(int y = -range + 1; y < range; y++){
                 if(x * x + y * y <= range * range){
                     owner = Vars.world.build(building.tileX() + x, building.tileY() + y);
-                    if(owner instanceof Temperaturec){
-                        if(((Temperaturec)owner).temperature() < building.temperature()){
-                            delta = building.temperature() - ((Temperaturec)owner).temperature();
+                    if(owner instanceof Temperaturec temperatureOwner){
+                        if(temperatureOwner.temperature() < building.temperature()){
+                            delta = building.temperature() - temperatureOwner.temperature();
                             if(delta > 1f){
                                 delta = 1f;
                             }
-                            owner.<Temperaturec>as().addTemperature(delta);
+                            temperatureOwner.addTemperature(delta);
                             building.addTemperature(-delta);
                         }
                     }
                 }
             }
         }
-        building.addTemperature(EndlessInterstellarVars.temperatureRegressionPerSecond / -60f);
+        owner = null;
+        building.addTemperature(-temperatureRegressionPerTic * Time.delta);
     }
 }
