9fa8dbbda4944e6fd9a941db7958698e201d59a9
Zelaux <ura20060605@gmail.com>
2022-08-03 04:38:07 +0300

    returned tools module

--- src/endint/EndlessInterstellar.java
+++ src/endint/EndlessInterstellar.java
@@ -1,21 +1,36 @@
 package endint;
 
-import endint.content.*;
-import mindustry.mod.Mod;
+import mindustry.ctype.*;
+import mma.*;
 import mma.annotations.ModAnnotations.*;
+import static endint.EndlessInterstellarVars.*;
 
 @MainClass
-public class EndlessInterstellar extends Mod {
-    public EndlessInterstellar(){
+public class EndlessInterstellar extends MMAMod {
 
+    public EndlessInterstellar() {
+        super();
+        EndlessInterstellarVars.load();
     }
 
     @Override
+    protected void modContent(Content content) {
+        super.modContent(content);
+    }
+
+    public void init() {
+        if (!loaded)
+            return;
+        super.init();
+        if (neededInit)
+            listener.init();
+    }
+
+    /**
+     * All content types should be loaded into loadContent from {@link EndlessInterstellarVars}
+     * There is you can load extra things like ModMusic or ModSounds etc.
+     */
     public void loadContent() {
-        ModItems.load();
-        ModLiquids.load();
-        ModRecipes.load();
-        ModBlocks.load();
-        ModPlanets.load();
+        super.loadContent();
     }
 }
