5b23f3c4c86993fb49b74eabbb19f4f665a5038d
Zelaux <ura20060605@gmail.com>
2022-08-11 22:43:25 +0300

    some updated#3

--- src/endint/math/ModMath.java
+++ src/endint/math/ModMath.java
@@ -2,7 +2,10 @@ package endint.math;
 
 public class ModMath {
     private static final String point = String.valueOf(1f/2f).substring(1, 2);
-
+    /**
+     * @deprecated can be replaced by Mathf.round(value, Math.pow(10,-max))
+    * */
+@Deprecated()
     public static float fixFloat(float value, int max){
         String temp = String.valueOf(value);
         return Float.parseFloat(temp.lastIndexOf(point) + max < temp.length() ? temp.substring(0, temp.lastIndexOf(point) + max + 1) : temp);
--- src/endint/world/blocks/distribution/DoubleConveyor.java
+++ src/endint/world/blocks/distribution/DoubleConveyor.java
@@ -1,39 +1,26 @@
 package endint.world.blocks.distribution;
 
-import arc.Core;
-import arc.func.Prov;
-import arc.graphics.Texture;
-import arc.graphics.g2d.Draw;
-import arc.graphics.g2d.TextureRegion;
-import arc.math.Mathf;
-import arc.math.geom.Vec2;
-import arc.struct.Seq;
-import arc.util.Eachable;
-import arc.util.Log;
-import arc.util.Tmp;
-import mindustry.content.Blocks;
-import mindustry.content.Items;
-import mindustry.entities.TargetPriority;
-import mindustry.entities.units.BuildPlan;
-import mindustry.gen.Building;
-import mindustry.gen.Sounds;
-import mindustry.type.Item;
-import mindustry.world.Block;
-import mindustry.world.blocks.distribution.ItemBridge;
-import mindustry.world.meta.BlockGroup;
-import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatUnit;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
 
 import static mindustry.Vars.itemSize;
 
-public class DoubleConveyor extends Block {
+public class DoubleConveyor extends Block{
     public float maxSpeed = 1f / 60f;
 
     public int capacity = 3;
 
     TextureRegion reg;
 
-    public DoubleConveyor(String name) {
+    public DoubleConveyor(String name){
         super(name);
         rotate = true;
         update = true;
@@ -56,7 +43,7 @@ public class DoubleConveyor extends Block {
         stats.add(Stat.itemsMoved, maxSpeed, StatUnit.itemsSecond);
     }
 
-    public class DoubleConveyorBuild extends Building {
+    public class DoubleConveyorBuild extends Building{
         public Seq<ConveyorItem> conveyorItems = new Seq<ConveyorItem>();
 
         protected Building tempBuild;
@@ -71,12 +58,12 @@ public class DoubleConveyor extends Block {
         }
 
         @Override
-        public void updateTile() {
+        public void updateTile(){
             conveyorItems.each(ConveyorItem::update);
         }
 
         @Override
-        public boolean acceptItem(Building source, Item item) {
+        public boolean acceptItem(Building source, Item item){
             if(source.left() == this || source.front() == this){
                 return conveyorItems.count(i -> i.place == 0) < capacity;
             }else if(source.right() == this || source.back() == this){
@@ -86,11 +73,11 @@ public class DoubleConveyor extends Block {
         }
 
         @Override
-        public void handleItem(Building source, Item item) {
+        public void handleItem(Building source, Item item){
             if(source.left() == this || source.front() == this){
-                conveyorItems.add(new ConveyorItem(item, () -> this, (short) 0));
+                conveyorItems.add(new ConveyorItem(item, (short)0));
             }else if(source.right() == this || source.back() == this){
-                conveyorItems.add(new ConveyorItem(item, () -> this, (short) 1));
+                conveyorItems.add(new ConveyorItem(item, (short)1));
             }
         }
 
@@ -103,58 +90,62 @@ public class DoubleConveyor extends Block {
 
             if(tempBuild.acceptItem(this, tempItem.item))
                 conveyorItems.remove(tempItem);
-                tempBuild.handleItem(this, tempItem.item);
+            tempBuild.handleItem(this, tempItem.item);
         }
 
         @Override
-        public void draw() {
+        public void draw(){
             super.draw();
             conveyorItems.each(ConveyorItem::draw);
         }
 
-        class ConveyorItem {
+        class ConveyorItem{
             public Item item;
-            public Prov<DoubleConveyorBuild> owner;
             public float progress = 0f;
             public short place;
 
             protected ConveyorItem tempItem;
 
-            public ConveyorItem(Item item, Prov<DoubleConveyorBuild> owner, short place){
+            public ConveyorItem(Item item, short place){
                 this.item = item;
-                this.owner = owner;
                 this.place = place;
             }
 
             public void update(){
-                if(progress >= 1f) {
-                    owner.get().dumpItem();
+                if(progress >= 1f){
+                    owner().dumpItem();
                     return;
                 }
 
                 tempItem = null;
 
-                owner.get().conveyorItems.each(i -> {
+                owner().conveyorItems.each(i -> {
                     if(i.place == place)
-                        if(i != this && Math.abs(i.progress - progress) <= progress / owner.get().getCapacity()
-                                && i.progress > progress) {
+                        if(i != this && Math.abs(i.progress - progress) <= progress / owner().getCapacity() && i.progress > progress){
                             tempItem = i;
                             Log.info(progress);
                         }
                 });
 
                 if(tempItem == null)
-                    progress += owner.get().getMaxSpeed();
+                    progress += owner().getMaxSpeed();
             }
 
             public void drawItem(){
-                float cos = Mathf.cosDeg(360-owner.get().rotation);
-                float sin = Mathf.sinDeg(360-owner.get().rotation);
+                float cos = Mathf.cosDeg(360 - owner().rotation);
+                float sin = Mathf.sinDeg(360 - owner().rotation);
 
-                Vec2 vec = new Vec2(place == 0 ? sin * (0.5f - progress) : sin * (0.5f + progress),
-                        place == 0 ? cos * (0.5f - progress) : cos * (0.5f + progress));
 
-                Draw.rect(item.fullIcon, owner.get().x + 4 + vec.x * 8, vec.y * 8 + owner.get().y + 4, itemSize, itemSize);
+//                Tmp.v1.trns(360-owner().rotation,progress)
+
+                float len = 0.5f + (place == 0 ? -progress : progress);
+                Vec2 vec = Tmp.v1.set(sin * len, cos * len);
+
+                Draw.rect(item.fullIcon, owner().x + 4 + vec.x * 8, vec.y * 8 + owner().y + 4, itemSize, itemSize);
+            }
+
+            private DoubleConveyorBuild owner(){
+                return DoubleConveyorBuild.this;
             }
 
             public void draw(){
